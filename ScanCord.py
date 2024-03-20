394b54ee94ea5add2fadcc11482788333d0e500b
Aravind Sri Sai Kisore <62432516+aravindcmd@users.noreply.github.com>
2024-03-20 12:47:47 +0100

    Update ScanCord.py

--- ScanCord.py
+++ ScanCord.py
@@ -1,15 +1,19 @@
 from pymycobot.mycobot import MyCobot
-
-res = []
+import time
+coord_list = []
 mc = MyCobot('/dev/ttyAMA0',1000000)
 for i in range(0,5):
-    mc.release_all_Servos()
+    mc.release_all_servos()
     print("scanning cord in 5 seconds", )
     time.sleep(5)
     mc.jog_stop()
-    res.append(mc.get_coords())
+    coord_list.append(mc.get_coords())
 
-print(res)
+print(coord_list)
+
+for i in coord_list:
+    print("changing cord in 3 seconds")
+    time.sleep(3)
+    mc.send_coords(coord_list[i],20,1)
+
 mc.release_all_servos()
-    
-    
