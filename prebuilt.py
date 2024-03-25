d4db78129ce46ac465cf603a1539d91737ee799f
Aravind Sri Sai Kisore <62432516+aravindcmd@users.noreply.github.com>
2024-03-25 11:41:34 +0100

    Update prebuilt.py

--- prebuilt.py
+++ prebuilt.py
@@ -7,16 +7,14 @@ import time
 #Enter the above code to import the packages required by the project
 
 def gripper_test(mc):
-
-print("Start check IO part of api\n")
+  print("Start check IO part of api\n")
 
 #Check if the gripper is moving
+  flag = mc.is_gripper_moving()
 
-flag = mc.is_gripper_moving()
+  print("Is gripper moving: {}".format(flag))
 
-print("Is gripper moving: {}".format(flag))
-
-time.sleep(1)
+  time.sleep(1)
 
 #Set the current position to (2048).
 
@@ -30,61 +28,61 @@ time.sleep(1)
 
 #Set joint point 1 to rotate to the position of 2048
 
-mc.set_encoder(1, 2048)
+  mc.set_encoder(1, 2048)
 
-time.sleep(2)
+  time.sleep(2)
 
 #Set six joint positions and let the robotic arm rotate to this position at a speed of 20
 
-mc.set_encoders([1024, 1024, 1024, 1024, 1024, 1024], 20)
+  mc.set_encoders([1024, 1024, 1024, 1024, 1024, 1024], 20)
 
-time.sleep(3)
+  time.sleep(3)
 
 #Get the position information of joint point 1
 
-print(mc.get_encoder(1))
+  print(mc.get_encoder(1))
 
 #Set the gripper to rotate to the position of 2048
 
-mc.set_encoder(7, 2048)
+  mc.set_encoder(7, 2048)
 
-time.sleep(3)
+  time.sleep(3)
 
 #Set the gripper to rotate to the position of 1300
 
-mc.set_encoder(7, 1300)
+  mc.set_encoder(7, 1300)
 
-time.sleep(3)
+  time.sleep(3)
 
 #Let the gripper reach the state of 2048 at a speed of 70
 
-mc.set_gripper_value(2048, 70)
+  mc.set_gripper_value(2048, 70)
 
-time.sleep(3)
+  time.sleep(3)
 
 #Let the gripper reach the state of 1500 at a speed of 70
 
-mc.set_gripper_value(1500, 70)
+  mc.set_gripper_value(1500, 70)
 
-time.sleep(3)
+  time.sleep(3)
 
 #Set the state of the gripper to quickly open the gripper at a speed of 70
 
-mc.set_gripper_state(0, 70)
+  mc.set_gripper_state(0, 70)
 
-time.sleep(3)
+  time.sleep(3)
 
 #Set the state of the gripper so that it quickly closes the gripper at a speed of 70
 
-mc.set_gripper_state(1, 70)
+  mc.set_gripper_state(1, 70)
 
-time.sleep(3)
+  time.sleep(3)
 
 #Get the value of the gripper
 
-print("")
+  print("")
 
-print(mc.get_gripper_value())
+  print(mc.get_gripper_value())
 
 if __name__ == "__main__":
 
@@ -127,13 +125,12 @@ if __name__ == "__main__":
 #Initialize a MyCobot object
 
 #Create object code here for Raspberry Pi version below
-
-mc = MyCobot(PI_PORT, PI_BAUD)
+  mc = MyCobot(PI_PORT, PI_BAUD)
 
 #make it move to zero position
 
-mc.set_encoders([2048, 2048, 2048, 2048, 2048, 2048], 20)
+  mc.set_encoders([2048, 2048, 2048, 2048, 2048, 2048], 20)
 
-time.sleep(3)
+  time.sleep(3)
 
-gripper_test(mc)
+  gripper_test(mc)
